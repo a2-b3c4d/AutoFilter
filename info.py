9bb5d22104422108f8cbce0871faac4255a7bb1b
Tharunfitfun <tharunfitfun204@gmail.com>
2025-07-06 16:23:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,12 +28,12 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://envs.sh/WdA.jpg https://envs.sh/Wdj.jpg')).split()  # Sample pic
-NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/Wdj.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/Wdj.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/Wdj.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/Wdj.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://envs.sh/Wdj.jpg')).split()  # Fsub pic
+PICS = (environ.get('PICS', 'https://envs.sh/F-9.mp4 https://files.catbox.moe/0kvknj.jpg')).split()  # Sample pic
+NOR_IMG = environ.get("NOR_IMG", "https://files.catbox.moe/w2phqb.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://files.catbox.moe/q2dcnh.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://files.catbox.moe/qv1ysp.jpg")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/whhe90.jpg'))
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://files.catbox.moe/rs5pl7.jpg')).split()  # Fsub pic
 
 # ============================
 # Admin, Channels & Users Configuration
