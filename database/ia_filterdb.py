e726c16aba9222d5b82836c59d29dd9c9e1326ce
Tharunfitfun <tharunfitfun204@gmail.com>
2025-07-12 14:11:54 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,319 +1,78 @@
-import logging
-from struct import pack
-import re
-import base64
-from pyrogram.file_id import FileId
-from pymongo.errors import DuplicateKeyError
-from umongo import Instance, Document, fields
-from motor.motor_asyncio import AsyncIOMotorClient
-from marshmallow.exceptions import ValidationError
-from info import CAPTION_LANGUAGES, DATABASE_URI, DATABASE_URI2, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN, MOVIE_UPDATE_CHANNEL, OWNERID
-from utils import get_settings, save_group_settings, temp, get_status
-from database.users_chats_db import add_name
-from .Imdbposter import get_movie_details, fetch_image
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import logging from struct import pack import re import base64 from pyrogram.file_id import FileId from pymongo.errors import DuplicateKeyError from umongo import Instance, Document, fields from motor.motor_asyncio import AsyncIOMotorClient from marshmallow.exceptions import ValidationError from info import CAPTION_LANGUAGES, DATABASE_URI, DATABASE_URI2, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN, MOVIE_UPDATE_CHANNEL, OWNERID from utils import get_settings, save_group_settings, temp, get_status from database.users_chats_db import add_name from .Imdbposter import get_movie_details, fetch_image from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-#---------------------------------------------------------
-# Some basic variables needed
-tempDict = {'indexDB': DATABASE_URI}
+logger = logging.getLogger(name) logger.setLevel(logging.INFO) #--------------------------------------------------------- tempDict = {'indexDB': DATABASE_URI}
 
-# Primary DB
-client = AsyncIOMotorClient(DATABASE_URI)
-db = client[DATABASE_NAME]
-instance = Instance.from_db(db)
+client = AsyncIOMotorClient(DATABASE_URI) db = client[DATABASE_NAME] instance = Instance.from_db(db)
 
-#secondary db
-client2 = AsyncIOMotorClient(DATABASE_URI2)
-db2 = client2[DATABASE_NAME]
-instance2 = Instance.from_db(db2)
+client2 = AsyncIOMotorClient(DATABASE_URI2) db2 = client2[DATABASE_NAME] instance2 = Instance.from_db(db2)
 
+@instance.register class Media(Document): file_id = fields.StrField(attribute='_id') file_ref = fields.StrField(allow_none=True) file_name = fields.StrField(required=True) file_size = fields.IntField(required=True) file_type = fields.StrField(allow_none=True) mime_type = fields.StrField(allow_none=True) caption = fields.StrField(allow_none=True)
 
-# Primary DB Model
-@instance.register
-class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
+class Meta:
+    indexes = ('$file_name', )
+    collection_name = COLLECTION_NAME
 
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
+@instance2.register class Media2(Document): file_id = fields.StrField(attribute='_id') file_ref = fields.StrField(allow_none=True) file_name = fields.StrField(required=True) file_size = fields.IntField(required=True) file_type = fields.StrField(allow_none=True) mime_type = fields.StrField(allow_none=True) caption = fields.StrField(allow_none=True)
 
-@instance2.register
-class Media2(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
+class Meta:
+    indexes = ('$file_name', )
+    collection_name = COLLECTION_NAME
 
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
+async def send_msg(bot, filename, caption): try: filename = re.sub(r'||\b@\S+|\bwww.\S+', '', filename).strip() caption = re.sub(r'||\b@\S+|\bwww.\S+', '', caption).strip()
 
-async def choose_mediaDB():
-    """This Function chooses which database to use based on the value of indexDB key in the dict tempDict."""
-    global saveMedia
-    if tempDict['indexDB'] == DATABASE_URI:
-        logger.info("Using first db (Media)")
-        saveMedia = Media
-    else:
-        logger.info("Using second db (Media2)")
-        saveMedia = Media2
+season, episodes = '', ''
+    match = re.findall(r"[Ss](\d+)[Ee](\d+)", filename)
+    if match:
+        season = f"s{match[0][0].zfill(2)}"
+        episodes = f"episode {match[0][1].zfill(2)}"
+    elif re.search(r"[Ss](\d+)", filename):
+        season = f"s{re.search(r'[Ss](\d+)', filename).group(1).zfill(2)}"
+    if "E" in filename and "&" in filename:
+        ep_text = filename.split("E")[-1]
+        if ep_text:
+            episodes = f"episodes {ep_text.strip()}"
 
-async def save_file(bot, media):
-  """Save file in database"""
-  global saveMedia
-  file_id, file_ref = unpack_new_file_id(media.file_id)
-  file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-  try:
-    if saveMedia == Media2: 
-        if await Media.count_documents({'file_id': file_id}, limit=1):
-            logger.warning(f'{file_name} is already saved in primary database!')
-            return False, 0
-    file = saveMedia(
-        file_id=file_id,
-        file_ref=file_ref,
-        file_name=file_name,
-        file_size=media.file_size,
-        file_type=media.file_type,
-        mime_type=media.mime_type,
-        caption=media.caption.html if media.caption else None,
-    )
-  except ValidationError:
-    logger.exception('Error occurred while saving file in database')
-    return False, 2
-  else:
-    try:
-      await file.commit()
-    except DuplicateKeyError:
-      logger.warning(f'{getattr(media, "file_name", "NO_FILE")} is already saved in database')   
-      return False, 0
-    else:
-        logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
-        if await get_status(bot.me.id):
-            await send_msg(bot, file.file_name, file.caption)
-        return True, 1
+    imdb = await get_movie_details(filename)
+    imdb_title = imdb.get("title") if imdb else None
+    rating = imdb.get("rating") if imdb else None
+    ott = imdb.get("ott") if imdb else None
+    poster_url = imdb.get("poster_url") if imdb else None
+    resized_poster = await fetch_image(poster_url) if poster_url else None
 
-async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
-    """For given query return (results, next_offset)"""
-    if chat_id is not None:
-        settings = await get_settings(int(chat_id))
-        try:
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
-        except KeyError:
-            await save_group_settings(int(chat_id), 'max_btn', False)
-            settings = await get_settings(int(chat_id))
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
-    query = query.strip()
-    if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
-    else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
-    
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
+    qualities = ["ORG", "org", "hdcam", "HDCAM", "HQ", "hq", "HDRip", "hdrip", "camrip", "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", "dvdrip", "dvdscr", "HDTC", "dvdscreen", "HDTS", "hdts"]
+    quality = await get_qualities(caption.lower(), qualities) or "HDRip"
 
-    if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
-    else:
-        filter = {'file_name': regex}
+    language = ""
+    for lang in CAPTION_LANGUAGES:
+        if lang.lower() in caption.lower():
+            language += f"{lang}, "
+    language = language[:-2] if language else "Not idea 😄"
 
-    if file_type:
-        filter['file_type'] = file_type
+    filename = re.sub(r"[\{\}:;'\-!]", "", filename)
 
-    total_results = ((await Media.count_documents(filter))+(await Media2.count_documents(filter)))
+    main_title = f"✅ {filename} {season} #𝚃𝚊𝚖𝚒𝚕"
+    text = f"{main_title}\n\n"
 
-    #verifies max_results is an even number or not
-    if max_results%2 != 0: 
-        logger.info(f"Since max_results is an odd number ({max_results}), bot will use {max_results+1} as max_results to make it even.")
-        max_results += 1
+    if episodes:
+        text += f"🍥 {episodes.capitalize()} Added\n\n"
+    text += f"👼 ɴᴀᴍᴇ: {filename}\n\n"
+    text += f"🫧 Qᴜᴀʟɪᴛʏ: {quality}\n\n"
+    text += f"🦋 ᴀᴜᴅɪᴏ: {language}\n\n"
+    if imdb_title:
+        text += f"✨ IMDB Info\n"
+    if ott:
+        text += f"📺 OTT: {ott}\n"
 
-    cursor = Media.find(filter)
-    cursor2 = Media2.find(filter)
+    filenames = filename.replace(" ", '-')
+    btn = [[InlineKeyboardButton("🌸 ᴄʟɪᴄᴋ ᴛᴏ ꜱᴇᴀʀᴄʜ 🌸", url=f"https://telegram.me/{temp.U_NAME}?start=getfile-{filenames}")]]
 
-    cursor.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
-
-    cursor2.skip(offset).limit(max_results)
-
-    fileList2 = await cursor2.to_list(length=max_results)
-    if len(fileList2)<max_results:
-        next_offset = offset+len(fileList2)
-        cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
-        cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
-        fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
-        files = fileList2+fileList1
-        next_offset = next_offset + len(fileList1)
-    else:
-        files = fileList2
-        next_offset = offset + max_results
-    if next_offset >= total_results:
-        next_offset = ''
-    return files, next_offset, total_results
-
-
-async def get_bad_files(query, file_type=None, filter=False):
-    """For given query return (results, next_offset)"""
-    query = query.strip()
-    if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
-    else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
-    
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
-
-    if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
-    else:
-        filter = {'file_name': regex}
-
-    if file_type:
-        filter['file_type'] = file_type
-
-    cursor = Media.find(filter)
-    cursor2 = Media2.find(filter)
-
-    cursor.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
-
-    files = ((await cursor2.to_list(length=(await Media2.count_documents(filter))))+(await cursor.to_list(length=(await Media.count_documents(filter)))))
-
-    total_results = len(files)
-
-    return files, total_results
-
-async def get_file_details(query):
-    filter = {'file_id': query}
-    cursor = Media.find(filter)
-    filedetails = await cursor.to_list(length=1)
-    if not filedetails:
-        cursor2 = Media2.find(filter)
-        filedetails = await cursor2.to_list(length=1)
-    return filedetails
-
-
-def encode_file_id(s: bytes) -> str:
-    r = b""
-    n = 0
-
-    for i in s + bytes([22]) + bytes([4]):
-        if i == 0:
-            n += 1
+    if await add_name(OWNERID, filename):
+        if resized_poster:
+            await bot.send_photo(chat_id=MOVIE_UPDATE_CHANNEL, photo=resized_poster, caption=text, reply_markup=InlineKeyboardMarkup(btn))
         else:
-            if n:
-                r += b"\x00" + bytes([n])
-                n = 0
+            await bot.send_message(chat_id=MOVIE_UPDATE_CHANNEL, text=text, reply_markup=InlineKeyboardMarkup(btn))
+except Exception as e:
+    logger.error(f"Error sending message: {e}")
 
-            r += bytes([i])
-
-    return base64.urlsafe_b64encode(r).decode().rstrip("=")
-
-def encode_file_ref(file_ref: bytes) -> str:
-    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
-
-def unpack_new_file_id(new_file_id):
-    """Return file_id, file_ref"""
-    decoded = FileId.decode(new_file_id)
-    file_id = encode_file_id(
-        pack(
-            "<iiqq",
-            int(decoded.file_type),
-            decoded.dc_id,
-            decoded.media_id,
-            decoded.access_hash
-        )
-    )
-    file_ref = encode_file_ref(decoded.file_reference)
-    return file_id, file_ref
-
-
-async def send_msg(bot, filename, caption): 
-    try:
-        filename = re.sub(r'\(\@\S+\)|\[\@\S+\]|\b@\S+|\bwww\.\S+', '', filename).strip()
-        caption = re.sub(r'\(\@\S+\)|\[\@\S+\]|\b@\S+|\bwww\.\S+', '', caption).strip()
-        
-        year_match = re.search(r"\b(19|20)\d{2}\b", caption)
-        year = year_match.group(0) if year_match else None
-
-        pattern = r"(?i)(?:s|season)0*(\d{1,2})"
-        season = re.search(pattern, caption) or re.search(pattern, filename)
-        season = season.group(1) if season else None 
-
-        if year:
-            filename = filename[: filename.find(year) + 4]  
-        elif season and season in filename:
-            filename = filename[: filename.find(season) + 1]
-
-        qualities = ["ORG", "org", "hdcam", "HDCAM", "HQ", "hq", "HDRip", "hdrip", "camrip", "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", "dvdrip", "dvdscr", "HDTC", "dvdscreen", "HDTS", "hdts"]
-        quality = await get_qualities(caption.lower(), qualities) or "HDRip"
-
-        language = ""
-        possible_languages = CAPTION_LANGUAGES
-        for lang in possible_languages:
-            if lang.lower() in caption.lower():
-                language += f"{lang}, "
-        language = language[:-2] if language else "Not idea 😄"
-
-        filename = re.sub(r"[\(\)\[\]\{\}:;'\-!]", "", filename)
-
-        text = "#𝑵𝒆𝒘_𝑭𝒊𝒍𝒆_𝑨𝒅𝒅𝒆𝒅 ✅\n\n👷𝑵𝒂𝒎𝒆: `{}`\n\n🌳𝑸𝒖𝒂𝒍𝒊𝒕𝒚: {}\n\n🍁𝑨𝒖𝒅𝒊𝒐: {}"
-        text = text.format(filename, quality, language)
-
-        if await add_name(OWNERID, filename):
-            imdb = await get_movie_details(filename)  
-            resized_poster = None
-
-            if imdb:
-                poster_url = imdb.get('poster_url')
-                if poster_url:
-                    resized_poster = await fetch_image(poster_url)  
-
-            filenames = filename.replace(" ", '-')
-            btn = [[InlineKeyboardButton('🌲 Get Files 🌲', url=f"https://telegram.me/{temp.U_NAME}?start=getfile-{filenames}")]]
-            
-            if resized_poster:
-                await bot.send_photo(chat_id=MOVIE_UPDATE_CHANNEL, photo=resized_poster, caption=text, reply_markup=InlineKeyboardMarkup(btn))
-            else:              
-                await bot.send_message(chat_id=MOVIE_UPDATE_CHANNEL, text=text, reply_markup=InlineKeyboardMarkup(btn))
-
-    except:
-        pass
-
-async def get_qualities(text, qualities: list):
-    """Get all Quality from text"""
-    quality = []
-    for q in qualities:
-        if q in text:
-            quality.append(q)
-    quality = ", ".join(quality)
-    return quality[:-2] if quality.endswith(", ") else quality
-
-
-
-
-
+async def get_qualities(text, qualities: list): quality = [q for q in qualities if q in text] quality = ", ".join(quality) return quality[:-2] if quality.endswith(", ") else quality
 
