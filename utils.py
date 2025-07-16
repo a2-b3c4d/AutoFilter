161cab773a127bdfbab54f24915e260ee84ed9f7
Tharunfitfun <tharunfitfun204@gmail.com>
2025-07-16 19:04:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -81,7 +81,7 @@ async def is_subscribed(bot, query, channels):
             await bot.get_chat_member(channel_id, query.from_user.id)
         except UserNotParticipant:
             btn.append(
-                [InlineKeyboardButton(f'❤️ {chat.title}', url=chat.invite_link)]
+                [InlineKeyboardButton(f'❤ {chat.title}', url=chat.invite_link)]
             )
         except Exception as e:
             pass
@@ -774,7 +774,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/SenpaiShift")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Momo_Ayase_bot")
                                 ]
                             ]
                         )
@@ -838,7 +838,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                 for file in files:
                     cap += f"<b><a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>📁 {get_size(file.file_size)} ▷ {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
     else:
-        cap = f"<b>🫧 ᴛɪᴛʟᴇ : <code>{search}</code>\n📦 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n👒 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n⏰ ᴛɪᴍᴇ ᴛᴏ ꜰɪɴᴅ : <code>{remaining_seconds} ꜱᴇᴄᴏɴᴅꜱ</code>\n🪄 ᴘᴏᴡᴇʀᴇᴅ ʙʏ : 👇\n🦋 <a href='https://t.me/TC_LinksZ'>ᴛᴄ_ʟɪɴᴋꜱᴢ</a>\n</b>"
+        cap = f"<b>🫧 ᴛɪᴛʟᴇ : <code>{search}</code>\n📦 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n👒 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n⏰ ᴛɪᴍᴇ ᴛᴏ ꜰɪɴᴅ : <code>{remaining_seconds} ꜱᴇᴄᴏɴᴅꜱ</code>\n🪄 ᴘᴏᴡᴇʀᴇᴅ ʙʏ : 👇\n🦋 <a href='https://t.me/+2JhghFkpIP0wMjNl'>ᴏʙʟɪᴠɪᴏɴ–⁶ ᴄᴏʀᴇ</a>\n</b>"
         cap+="\n\n<b>🌌 <u>ʏᴏᴜʀ ᴇɴᴄʜᴀɴᴛᴇᴅ ꜰɪʟᴇꜱ ᴀᴡᴀɪᴛ ✨</u> 👇\n\n</b>"
         for file in files:
             cap += f"<b><a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>📁 {get_size(file.file_size)} ▷ {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
